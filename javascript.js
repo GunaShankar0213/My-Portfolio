fda4f0cac7b0399a517e270dc2fa29a9aef43ba8
GunaShankar0213 <103201002+GunaShankar0213@users.noreply.github.com>
2022-05-17 15:37:35 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    })
+    });
+    
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Student", "Developer", "Programmer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Student", "Developer", "Programmer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    
+     // owl carousel script
+     $('.carousel').owlCarousel({
+        margin: 20,
+        loop: false,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
